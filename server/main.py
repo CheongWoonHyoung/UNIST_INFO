7562fab8a07acd0f65797717db30dbd9c88fb82b
InJung Chung <mu29gl@gmail.com>
2015-09-20 13:23:04 +0900

    make announce function

--- server/main.py
+++ server/main.py
@@ -1,6 +1,27 @@
 # -*- coding: utf-8 -*-
 
-from server import *
+from app import *
+from database import db
+
+@app.route('/')
+def index():
+    return "hi"
+
+@app.route('/announce/', methods=['POST', 'GET'])
+def announce():
+    # POST : 글 작성
+    if request.method == 'POST':
+        return ""
+
+    # GET : 글 가져오기
+    cursor = db.execute("SELECT * FROM `announce`;")
+    data = cursor.fetchall()
+    print data
+    return json.dumps(data)
+
+def get_json(cursor):
+    return json.dumps([ dict(line) for line in [zip([ column[0] for column in
+                     cursor.description], row) for row in cursor.fetchall()] ])
 
 if __name__ == '__main__':
     init_server()
