12220afcbccd7a91c6844359dd47ac057848b7dc
InJung Chung <mu29gl@gmail.com>
2015-09-29 16:42:56 +0900

    데이터 삽입

--- server/main.py
+++ server/main.py
@@ -11,13 +11,19 @@ def index():
 def announce():
     # POST : 글 작성
     if request.method == 'POST':
-        return ""
+        cursor = db.execute("INSERT INTO `announce` (`title`, `content`) " +
+                            "VALUES ('" + request.form['title'] +
+                            "', '" + request.form['content'] + "');")
+        cursor.close()
+        return '{result:"success"}'
 
-    # GET : 글 가져오기
     #cursor = db.execute("SELECT * FROM `announce`;")
     #data = cursor.fetchall()
-    ##print data
-    return render_template('announce.html')#json.dumps(data)
+    #print data
+    #json.dumps(data)
+
+    # GET : 공지사항 작성하기
+    return render_template('announce.html')
 
 def get_json(cursor):
     return json.dumps([ dict(line) for line in [zip([ column[0] for column in
