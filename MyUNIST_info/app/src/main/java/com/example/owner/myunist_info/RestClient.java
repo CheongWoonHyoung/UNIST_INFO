3f5f1426298112179dbad5021e76fdb880db981c
InJung Chung <mu29gl@gmail.com>
2015-09-30 13:43:22 +0900

    fix RestClient

--- MyUNIST_info/app/src/main/java/com/example/owner/myunist_info/RestClient.java
+++ MyUNIST_info/app/src/main/java/com/example/owner/myunist_info/RestClient.java
@@ -1,8 +1,14 @@
 package com.example.owner.myunist_info;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.util.List;
 
-import retrofit.Retrofit;
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.converter.GsonConverter;
 import retrofit.http.GET;
 import retrofit.http.Path;
 
@@ -14,14 +20,17 @@ public class RestClient {
     private static RestClient client;
 
     public RestClient() {
-        Retrofit restAdapter = new Retrofit.Builder()
-                .baseUrl("https://api.github.com")
+        Gson gson = new GsonBuilder().create();
+
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint("http://10.0.1.4:5000")
+                .setConverter(new GsonConverter(gson))
                 .build();
 
         service = restAdapter.create(UNISTService.class);
     }
 
-    public synchronized UNISTService getService() {
+    public static synchronized UNISTService getService() {
         if (client == null)
             client = new RestClient();
 
@@ -30,6 +39,6 @@ public class RestClient {
 
     public interface UNISTService {
         @GET("/list")
-        List<Announce> getAnnounces();
+        void getAnnounces(Callback<List<Announce>> callback);
     }
 }
