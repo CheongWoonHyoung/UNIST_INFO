dd40bdcf50bb392d8983b5ee050471672d2225f1
InJung Chung <mu29gl@gmail.com>
2015-09-30 10:39:16 +0900

    Announce 클래스

---
+++ MyUNIST_info/app/src/main/java/com/example/owner/myunist_info/Announce.java
@@ -0,0 +1,22 @@
+package com.example.owner.myunist_info;
+
+/**
+ * Created by InJung on 2015. 9. 29..
+ */
+public class Announce {
+    int id;
+    String title;
+    String content;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
